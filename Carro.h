186a9e6d5ed611efc2d1aee355e776bb58362fd7
jmurteira <46197229+jmurteira@users.noreply.github.com>
2019-11-14 17:17:50 +0000

    carro.h

--- Carro.h
+++ Carro.h
@@ -1,9 +1,41 @@
 #pragma once
+#include <string>
+using namespace std;
+
 class Carro
 {
+	string marca;
+	string modelo;
+	int ident;			//identificador do carro
+	float energia;
+	float energiaMax;
+	int velocidade;
+	int velMax;
+	int cronometro;		//tempo em segudos
+	bool estado;
+	bool sinalEmerg;
+	bool avariado;
+
 public:
 	Carro();
 	~Carro();
-	//testar alter
+	
+	//ideia das funções
+	//Velocidade
+	void acelera();
+	void trava();
+	int getVelocidade();
+
+	//Energia
+	void gastaEnergia();
+	float getEnergia();
+
+	//Botao Emergencia
+	void BotaoSOS();
+
+
+
+
+
 };
 
