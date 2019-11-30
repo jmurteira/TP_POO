87bf9715a3812455bcba1e01135db1ab6e5de36c
jonasmigas <34393594+jonasmigas@users.noreply.github.com>
2019-11-30 05:55:07 +0000

    mais alguma info (ver interface)

--- Interface.cpp
+++ Interface.cpp
@@ -25,6 +25,14 @@ Dga* Interface::getDga() {
 
 void Interface::Simulador() {
 	//imprimir as merdas todas bonitas da consola bla bla
+	//talvez aparecer primeiro um "intro" a dizer Car Simulator e carregar qualquer tecla para entrar
+	//depois apareceria uma lista de comandos, com uma distincao dos modos
+	//assim que fosse escolhido o Campeonato, apareceria a pista e a garagem do autodromo e apenas os comandos do modo 2 (talvez)
+	//o campeonato pede o numero de carros que vao para a pista
+	//o mesmo mas para a garagem
+	//e a corrida começa a partir do momento em que todos os carros da pista têm piloto
+	//acaba corrida, aparece classificacao final //maybe
+	//carregar qualquer tecla para voltar ao menu inicial
 	Consola::setBackgroundColor(Consola::BRANCO_CLARO);
 	Consola::setTextColor(Consola::PRETO);
 
